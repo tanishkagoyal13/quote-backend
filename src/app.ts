2ed56d847fb6840d3150587cf4d221db5b1857a6
Tanishka Goyal <tanishkagoyal1302@gmail.com>
2026-06-27 01:02:10 +0530

    feat: add request logging and request ID middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application } from 'express';
 import quoteRoutes from './routes/quoteRoutes';
 import { errorHandler } from './middleware/errorHandler';
+import { requestLogger } from './middleware/logger';
+import { requestId } from './middleware/requestId';
 
 const app: Application = express();
 
 // Middleware
 app.use(express.json());
+app.use(requestId);
+app.use(requestLogger);
 
 // Routes
 app.use('/quotes', quoteRoutes);
---
+++ src/middleware/requestId.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+import { randomUUID } from 'crypto';
+
+export const requestId = (req: Request, res: Response, next: NextFunction) => {
+  const id = randomUUID();
+  req.headers['x-request-id'] = id;
+  res.setHeader('X-Request-ID', id);
+  next();
+};
